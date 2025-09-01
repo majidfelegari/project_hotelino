a4332f45da7896a0935a49383fcfeac98b6400c0
majid <f.majid1377@gmail.com>
2025-09-01 16:26:44 +0330

    botton nav part one

--- lib/routes/main_botton_nav.dart
+++ lib/routes/main_botton_nav.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/routes/test.dart';
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class MainBottonNav extends StatefulWidget {
@@ -9,8 +10,31 @@ class MainBottonNav extends StatefulWidget {
 }
 
 class _MainBottonNavState extends State<MainBottonNav> {
+  late PersistentTabController _controller;
+  
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+  }
+
+  _buildScreens(){
+    return [
+      HomePage(),
+      FovoritePage(),
+      BookingPage(),
+      ProfilePage(),
+    ];
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PersistentTabView(
+      context, 
+      controller: _controller,
+      onItemSelected: (value) {},
+      screens: [],
+      );
   }
 }
