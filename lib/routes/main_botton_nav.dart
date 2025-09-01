8dc4c6b268b796c9b6f2e3c5c93c521e25fc759b
majid <f.majid1377@gmail.com>
2025-09-01 16:26:05 +0330

    add persistent botton nav lib and botton nav part one

---
+++ lib/routes/main_botton_nav.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
+
+class MainBottonNav extends StatefulWidget {
+  const MainBottonNav({super.key});
+
+  @override
+  State<MainBottonNav> createState() => _MainBottonNavState();
+}
+
+class _MainBottonNavState extends State<MainBottonNav> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/routes/test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+class FovoritePage extends StatelessWidget {
+  const FovoritePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
+class BookingPage extends StatelessWidget {
+  const BookingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
+
+
+
+
