c87f68cf120ff82137a4682ee782846c29529c71
majid <f.majid1377@gmail.com>
2025-09-02 10:21:05 +0330

    add home appbar

--- lib/features/home/homePage.dart
+++ lib/features/home/homePage.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/features/home/presentation/provider/home_provider.dart';
+import 'package:hotelino/features/home/presentation/widgets/home_appbar.dart';
+import 'package:provider/provider.dart';
 
-class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final homeProvider = Provider.of<HomeProvider>(context);
+    return Scaffold(
+      appBar: HomeAppBar(),
+      body: Center(),
+    );
   }
 }
