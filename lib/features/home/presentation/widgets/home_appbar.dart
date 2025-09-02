77ea55abaab780f1c38fbfecdfcae3de9694c359
majid <f.majid1377@gmail.com>
2025-09-02 11:40:49 +0330

    fix profile provider and finish home app bar

--- lib/features/home/presentation/widgets/home_appbar.dart
+++ lib/features/home/presentation/widgets/home_appbar.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hotelino/core/theme/theme_provider.dart';
+import 'package:hotelino/features/home/presentation/provider/profile_provider.dart';
+import 'package:provider/provider.dart';
 
-class HomeAppBar extends StatelessWidget implements PreferredSizeWidget{
+class HomeAppBar extends StatelessWidget implements PreferredSizeWidget {
   const HomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return AppBar(
       elevation: 0,
       automaticallyImplyLeading: false,
@@ -16,39 +21,68 @@ class HomeAppBar extends StatelessWidget implements PreferredSizeWidget{
               Stack(
                 children: [
                   IconButton(
-                    onPressed: () {  }, 
-                    icon: Icon(
-                      Icons.notifications_none,
-                      color: Colors.grey,
-                    ))
+                    onPressed: () {},
+                    icon: Icon(Icons.notifications_none, color: Colors.grey),
+                  ),
+                  Consumer<ProfileProvider>(
+                    builder: (context, profileProvider, child) {
+                      return profileProvider.profile?.notifications != null &&
+                              profileProvider.profile!.notifications > 0
+                          ? Positioned(
+                              right: 12,
+                              top: 12,
+                              child: CircleAvatar(
+                                radius: 4,
+                                backgroundColor: Theme.of(
+                                  context,
+                                ).colorScheme.primary,
+                              ),
+                            )
+                          : SizedBox();
+                    },
+                  ),
                 ],
-              )
+              ),
+              IconButton(
+                onPressed: () {
+                  themeProvider.toggleTheme();
+                },
+                icon: Icon(
+                  themeProvider.brightness == Brightness.light
+                      ? Icons.dark_mode
+                      : Icons.light_mode,
+                  color: Colors.grey,
+                ),
+              ),
             ],
           ),
 
-          Row(
-            children: [
-              Text(
-                'کاربر',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              const SizedBox(
-                width: 8,
-              ),
-              CircleAvatar(
-                radius: 20,
-                backgroundImage: NetworkImage("https://www.w3schools.com/howto/img_avatar.png"),
-              )
-            ],
-          )
+          Consumer<ProfileProvider>(
+            builder: (context, profileProvider, child) {
+              return Row(
+                children: [
+                  Text(
+                    profileProvider.profile?.name ?? "کاربر",
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(width: 8),
+                  CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                      profileProvider.profile?.avatarUrl ??
+                          "https://www.w3schools.com/howto/img_avatar.png",
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ],
       ),
     );
   }
-  
+
   @override
   // TODO: implement preferredSize
   Size get preferredSize => const Size.fromHeight(56);
-
-
 }
