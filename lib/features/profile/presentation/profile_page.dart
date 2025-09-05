bbb2ac5de8a72190f7429fb652cf6b0092bc1609
majid <f.majid1377@gmail.com>
2025-09-05 23:35:03 +0330

    add upper sections of profile

--- lib/features/profile/presentation/profile_page.dart
+++ lib/features/profile/presentation/profile_page.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:hotelino/features/home/presentation/provider/profile_provider.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -26,6 +28,68 @@ class _ProfilePageState extends State<ProfilePage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Consumer<ProfileProvider>(
+      builder: (context, profileProvider, child) {
+        final profile = profileProvider.profile;
+
+        if (profile == null) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        return Scaffold(
+          body: SafeArea(child: SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 24,
+                ),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 55,
+                      backgroundImage: _image != null
+                      ? FileImage(_image!)
+                      : NetworkImage(profile.avatarUrl) as ImageProvider,
+                    ),
+                    Positioned(
+                      right: 0,
+                      bottom: 0,
+                      child: GestureDetector(
+                        onTap: _pickImage,
+                        child: Container(
+                          padding: EdgeInsets.all(6),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).colorScheme.primaryFixed,
+                            shape: BoxShape.circle
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            size: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 12,),
+                Text(
+                  profile.name,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                SizedBox(height: 4,),
+                Text(
+                  profile.email,
+                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(color: Colors.grey),
+                ),
+                SizedBox(
+                  height: 24,
+                ),
+              ],
+            ),
+          )),
+        );
+      },
+    );
   }
 }
