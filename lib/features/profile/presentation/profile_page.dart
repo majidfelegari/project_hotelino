58b69b2fcc6ccb5e389d6a0c95b47723bc384b22
majid <f.majid1377@gmail.com>
2025-09-05 20:43:32 +0330

    add image from gallery using image_picker

---
+++ lib/features/profile/presentation/profile_page.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  File? _image;
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+
+    if (picked != null) {
+      setState(() {
+        _image = File(picked.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/routes/main_botton_nav.dart
+++ lib/routes/main_botton_nav.dart
@@ -5,7 +5,8 @@ import 'package:hotelino/core/utils/kyboard.dart';
 import 'package:hotelino/features/booking/presentation/booking_page.dart';
 import 'package:hotelino/features/favorite/presentation/favorite_page.dart';
 import 'package:hotelino/features/home/presentation/homePage.dart';
-import 'package:hotelino/routes/test.dart';
+import 'package:hotelino/features/profile/presentation/profile_page.dart';
+
 import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class MainBottonNav extends StatefulWidget {
