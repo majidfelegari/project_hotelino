49b8b6597f007dec628d4b2a56b2cfcd113bb04c
majid <f.majid1377@gmail.com>
2025-09-06 16:39:15 +0330

    add name and address hotel in hotel_detail_page

--- lib/features/hotel_detail/presentation/hotel_detail_page.dart
+++ lib/features/hotel_detail/presentation/hotel_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:hotelino/core/utils/network.dart';
 import 'package:hotelino/features/home/data/models/hotel.dart';
 import 'package:hotelino/features/home/repositories/hotel_repository.dart';
 import 'package:hotelino/shared/services/json_data_service.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class HotelDetailPage extends StatelessWidget {
   const HotelDetailPage({super.key, required this.hotelId});
@@ -19,29 +20,71 @@ class HotelDetailPage extends StatelessWidget {
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
+            body: Center(child: CircularProgressIndicator()),
           );
         }
 
         final hotel = snapshot.data!;
         return Scaffold(
           body: CustomScrollView(
-
             slivers: [
               SliverAppBar(
                 floating: true,
                 pinned: false,
                 flexibleSpace: FlexibleSpaceBar(
                   background: GestureDetector(
-                    onLongPress: () {
-                      
-                    },
+                    onLongPress: () {},
                     child: Image.network(
                       fit: BoxFit.cover,
-                      networkUrl(hotel.images.first)
+                      networkUrl(hotel.images.first),
                     ),
+                  ),
+                ),
+                elevation: 8,
+                expandedHeight: 300,
+                leading: IconButton(
+                  onPressed: () {
+                    PersistentNavBarNavigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 12, vertical: 14),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        hotel.name,
+                        style: textTheme.headlineMedium,
+                        textDirection: TextDirection.rtl,
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                            child: Text(
+                              hotel.address,
+                              textAlign: TextAlign.right,
+                              textDirection: TextDirection.rtl,
+                              style: TextStyle(fontSize: 14, color: Colors.grey),
+                            ),
+                          ),
+                          SizedBox(width: 6,),
+                          Icon(
+                            Icons.location_on_outlined,
+                            size: 18,
+                            color: Colors.grey,
+                          )
+                        ],
+                      )
+                    ],
                   ),
                 ),
               )
@@ -49,6 +92,6 @@ class HotelDetailPage extends StatelessWidget {
           ),
         );
       },
-      );
+    );
   }
 }
